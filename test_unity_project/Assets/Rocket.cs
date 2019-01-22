c8b6e924a744cc808232930fdc4bf0e79b360f1f
Sean Efe Yilmaz <efey1998@gmail.com>
2019-01-22 15:07:51 -0500

    Prefabs & Thrust Sound

--- test_unity_project/Assets/Rocket.cs
+++ test_unity_project/Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start() {
-        rigidBody = GetComponent<Rigidbody>();  
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource =  GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,16 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+        
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
